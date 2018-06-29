d8b53ec1b4ce0637b634a9c4d3e319259e5ce347
Calvin <mrcalvinkc@gmail.com>
2018-06-28 20:44:13 -0700

    Changed counter-example to use localStorage with component lifecycles

--- src/playground/counter-example.js
+++ src/playground/counter-example.js
@@ -5,8 +5,28 @@ class Counter extends React.Component {
     this.handleSubtractOne = this.handleSubtractOne.bind(this);
     this.handleResetAll = this.handleResetAll.bind(this);
     this.state = {
-      count: props.count
+      count: 0
     };
+  }
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.count !== this.state.count) {
+      localStorage.setItem('count', this.state.count);
+    }
+  }
+  componentDidMount(){
+    try {
+      const stringCount = localStorage.getItem('count');
+      const count = parseInt(stringCount, 10);
+
+      if (!isNaN(count)) {
+        this.setState(() => ({ count }));
+      }
+
+      console.log('Component Mounted: Fetching Data');
+
+    } catch (e) {
+      // do nothing, but at least caught the edge case.
+    }
   }
   handleAddOne() {
     this.setState((prevState) => {
@@ -41,40 +61,4 @@ class Counter extends React.Component {
   }
 }
 
-Counter.defaultProps = {
-  count: 9
-};
-
 ReactDOM.render(<Counter count={5}/>, document.getElementById('app'));
-
-// let count = 0;
-// const addOne = () => {
-//   count++;
-//   renderCounterApp();
-// };
-// const minusOne = () => {
-//   count--;
-//   renderCounterApp();
-// };
-// const reset = () => {
-//   count = 0;
-//   renderCounterApp();
-// };
-
-
-// const appRoot = document.getElementById('app');
-
-// const renderCounterApp = () => {
-//   const templateTwo = (
-//   <div>
-//     <h1>Count: {count}</h1>
-//     <button onClick={addOne}>+1</button>
-//     <button onClick={minusOne}>-1</button>
-//     <button onClick={reset}>Reset</button>
-//   </div>
-//   );
-
-//   ReactDOM.render(templateTwo, appRoot);
-// };
-
-// renderCounterApp();
