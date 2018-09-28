c95d1789a8fca0c4426e60abfd6f7f4c3143d7dd
Calvin <mrcalvinkc@gmail.com>
2018-09-27 18:32:03 -0700

    Style changes

--- src/components/Option.js
+++ src/components/Option.js
@@ -4,6 +4,7 @@ const Option = (props) => (
     <div>
       {props.optionText.concat("  ")} 
       <button
+        className="button button-link"
         onClick={(e) => {
           props.handleDeleteOption(props.optionText);
         }}
