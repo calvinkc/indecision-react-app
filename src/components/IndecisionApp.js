3a415e748828f07728bfff9593459480982439a3
Calvin <mrcalvinkc@gmail.com>
2018-09-27 20:25:35 -0700

    Further styling changes

--- src/components/IndecisionApp.js
+++ src/components/IndecisionApp.js
@@ -59,7 +59,7 @@ export default class IndecisionApp extends React.Component {
 
   handleAddOption = (option) => {
     if (!option) { 
-      return 'Please enter something';
+      return 'Please enter valid value to add item';
     } else if (this.state.options.indexOf(option) > -1) {
       return 'This option already exists';
     }
@@ -77,14 +77,16 @@ export default class IndecisionApp extends React.Component {
           hasOptions={this.state.options.length > 0}
           handlePick={this.handlePick}
         />
-        <Options 
-          options={this.state.options} 
-          handleDeleteOptions={this.handleDeleteOptions}
-          handleDeleteOption={this.handleDeleteOption}
-        />
-        <AddOption 
-          handleAddOption={this.handleAddOption}
-        />
+        <div className="widget">
+          <Options 
+            options={this.state.options} 
+            handleDeleteOptions={this.handleDeleteOptions}
+            handleDeleteOption={this.handleDeleteOption}
+          />
+          <AddOption 
+            handleAddOption={this.handleAddOption}
+          />
+          </div>
         </div>
         <OptionModal
           selectedOption={this.state.selectedOption}
